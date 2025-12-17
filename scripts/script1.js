a8212449363fbf049107220c3b3acfb2768edef6
Marina-oops <79970496+Marina-oops@users.noreply.github.com>
2025-12-18 02:14:13 +0300

    Update script1.js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,7 +1,7 @@
 // Конфигурация API и путей к изображениям
 const CONFIG = {
     API: {
-        BASE_URL: 'https://sputnik-db.dotindex-team.ru', // Замените на ваш URL бекенда
+        BASE_URL: 'https://corsproxy.io/?' + encodeURIComponent('https://sputnik-db.dotindex-team.ru'),
         ENDPOINTS: {
             PROFILES: '/long_poll_profiles/'
         }
